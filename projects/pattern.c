e46240b2439175483b9cb61e3d8325bc38ae1dec
Neil Thapa <thapaa.neill@gmail.com>
2025-12-19 11:19:21 +0545

    feat: refactor pattern.c into functions and add pyramid pattern

--- projects/pattern.c
+++ projects/pattern.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+void left_aligned_pattern(int number);
+void right_aligned_pattern(int number);
+void pyramid(int number);
+
 int main()
 {
     int integer;
@@ -9,10 +13,65 @@ int main()
         scanf("%d", &integer);
     } 
     while (integer < 1 || integer > 10);
+    
+    printf("The left aligned pattern of numbers:\n");
+    left_aligned_pattern(integer);
 
-    for (int i = 0; i < integer; i++)
+    printf("The right aligned pattern of numbers:\n");
+    right_aligned_pattern(integer);
+
+    printf("This is the pyramid: \n");
+    pyramid(integer);
+
+    return 0;
+}
+
+void left_aligned_pattern(int number)
+{
+    for (int i = 0; i < number; i++)
     {
         for (int j = 0; j < i + 1; j++)
+        {
+            printf("%d", i + 1);
+        }
+        printf("\n");
+    } 
+    printf("\n");
+}
+
+void right_aligned_pattern(int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        for (int j1 = 0; j1 < number - i - 1; j1++)
+        {
+            printf(" ");
+        }
+        for (int j2 = 0; j2 < i + 1; j2++)
+        {
+            printf("%d", i + 1);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void pyramid(int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        for (int j1 = 0; j1 < number - i - 1; j1++)
+        {
+            printf(" ");
+        }
+        for (int j2 = 0; j2 < i + 1; j2++)
+        {
+            printf("%d", i + 1);
+        }
+        
+        printf("  ");
+
+        for (int j3 = 0; j3 < i + 1; j3++)
         {
             printf("%d", i + 1);
         }
