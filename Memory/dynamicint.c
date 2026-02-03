0f55eb075b87ae3f0a0bd554b55124526a6017d6
Neil Thapa <thapaa.neill@gmail.com>
2026-02-03 13:41:52 +0000

    feat(Memory): add sum and average calculation for dynamic array

--- Memory/dynamicint.c
+++ Memory/dynamicint.c
@@ -2,13 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void increase_capacity(int **arr, int *size, int *capacity);
+void increase_capacity(int **arr, int size, int *capacity);
+int calculate_sum(int arr[], int size);
+float calculate_average(int elements_sum, int size);
 
 int main(void)
 {   
     int n;
-    printf("How many integers do you want to enter?: ");
-    scanf("%d", &n);
+    do 
+    {
+        printf("How many integers do you want to enter?: ");
+        scanf("%d", &n);
+    }
+    while (n < 1);
 
     // states
     int capacity = 2; // total elements that the array can hold initially
@@ -25,12 +31,12 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         printf("Number %d?: ", i + 1);
-        increase_capacity(&ptr, &size, &capacity); // automatically increases size if required
+        increase_capacity(&ptr, size, &capacity); // automatically increases size if required
         scanf("%d", ptr + size);
         size++; // update number of elements
     }
 
-    //print the elements
+    // print the elements
     printf("The elements in array:\n");
     for (int i = 0; i < n; i++)
     {
@@ -38,23 +44,49 @@ int main(void)
     }
     printf("\n");
 
+    // function call for calculating sum and average
+    int sum = calculate_sum(ptr, size);
+    printf("The sum of elements in the array: %d\n", sum);
+    printf("The avearge: %.2f\n", calculate_average(sum, size));
+
     free(ptr); // free memory
 
     return 0;
 }
 
-void increase_capacity(int **arr, int *size, int *capacity)
+void increase_capacity(int **arr, int size, int *capacity)
 {
-    if (*size == *capacity)
+    if (size == *capacity)
     {
         *capacity *= 2;
         int *temp = realloc(*arr, (*capacity) * sizeof(int)); // expand memory 
         if (temp == NULL)
         {
-            free(*arr);
             return;
         }
 
         *arr = temp;
     }
+}
+
+int calculate_sum(int arr[], int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+float calculate_average(int elements_sum, int size)
+{
+    if (size == 0)
+    {
+        return 0.0f; // f indicates it is a float literal not double
+    }
+    float average = (float) elements_sum / size;
+    return average;
+    
 }
