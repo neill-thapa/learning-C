703445b076ac6897bf2dae743927443520e2cc46
Neil Thapa <thapaa.neill@gmail.com>
2026-02-05 02:51:18 +0000

    feat(twoDimensionalArray): add a simple 2d array program

---
+++ twoDimensionalArray/print.c
@@ -0,0 +1,29 @@
+// basic 2d arrray program that stores and prints the elements of the array
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[2][3]; 
+
+    printf("Enter 6 numbers:\n");
+    
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("Matrix:\n");
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
