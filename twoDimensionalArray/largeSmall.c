119228b16161c78d3a1f23e067fcf101132a4bb2
Neil Thapa <thapaa.neill@gmail.com>
2026-02-07 11:54:34 +0000

    feat(twoDimensionalArray): add 2D array program to find largest and smallest values
    - takes input for rows and columns from the user
    - array is declared on the basis of user input
    - user populates the array and largest and smallest values are determined and finally printed

---
+++ twoDimensionalArray/largeSmall.c
@@ -0,0 +1,55 @@
+// program to find the largest and the smallest element in 2d array
+#include <stdio.h>
+
+int main(void)
+{
+    int rows, cols;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    if (rows <= 0 || cols <= 0)
+    {
+        printf("Invalid matrix size.\n");
+        return 1;
+    }
+
+    // declare and populate the array
+    int arr[rows][cols];
+    printf("Enter the numbers in the array:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("arr[%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    // determine largest and smallest
+    int largest = arr[0][0], smallest = arr[0][0];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (arr[i][j] > largest)
+            {
+                largest = arr[i][j];
+            }
+
+            if (arr[i][j] < smallest)
+            {
+                smallest = arr[i][j];
+            }
+        }
+    }
+
+    // print the numbers
+    printf("The largest number in the array: %d\n", largest);
+    printf("The smallest number in the array: %d\n", smallest);
+
+    return 0;
+}
